a4a42cc0e3beb846e93d4ee7e3942f24f4d28658
Paolo Veglia <pveglia@gmail.com>
2012-12-02 14:48:45 +0000

    first commit

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject yaramp "0.1.0-SNAPSHOT"
+            :description "FIXME: write this!"
+            :dependencies [[org.clojure/clojure "1.4.0"]
+                           [noir "1.2.2"]]
+            :main yaramp.server)
+
